ce624557fd64561a4f2c1e6705fe95731a580487
Dhika <yokim051019@gmail.com>
2024-10-17 02:34:11 +0700

    update landing client

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Sahabat Palabuan</title>
+  <title>Siyay</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
